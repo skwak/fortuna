0510d36958e45acbefdc3a1bf70508812754eab8
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-11-02 15:55:26 -0800

    add fortune model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     @questions = Question.all
     @question = Question.new
+    @fortune = Fortune.fortunes_array.sample
   end
 
 end
---
+++ app/models/fortune.rb
@@ -0,0 +1,8 @@
+class Fortune
+  attr_accessor :fortunes_array
+
+  def initialize
+    @fortunes_array = ["Absolutely.", "Certainly.", "Of course.", "Uh, probably.", "What are you talking about?", "Duh!", "Definitely not.", "Sure thing.", "What did you say?", "No.", "Absolutely not.", "Why are you asking me this?", "That's a dumb question. You're an idiot."]
+  end
+
+end

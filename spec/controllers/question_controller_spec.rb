7ff415c1f475272029f410fdfdb5c8f3b3942420
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-11-02 15:26:00 -0800

    add tests

--- spec/controllers/question_controller_spec.rb
+++ spec/controllers/question_controller_spec.rb
@@ -8,13 +8,33 @@ describe QuestionsController do
   subject { "questions#index" }
 
   it "should get index" do
-    get "index"
+    get :index
+    expect(response).to be_success
   end
 
-  subject { "questions#new"}
+  it "responds successfully with an HTTP 200 status code" do
+    get :index
+    expect(response).to be_success
+    expect(response).to have_http_status(200)
+  end
+
+  it "loads all of the questions into @questions" do
+      question1, question2 = Question.create!(author: "blah", body: "alwkerja;lrekwr"), Question.create!(author: "zoom", body: "a;lwerkajw;rlkwaer")
+      get :index
+
+      expect(assigns(:questions)).to match_array([question1, question2])
+  end
+
+  subject { "questions#new" }
 
   it "should get new form" do
     get "new"
+  end
+
+  subject { "questions#create" }
+
+  it "should redirect to home_path" do
+    post "create"
   end
 
 

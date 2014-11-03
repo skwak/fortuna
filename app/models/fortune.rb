577a596e15eb0a08037e903fe87124be64bba3b9
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-11-02 16:36:28 -0800

    add more fortunes; edit view pages with css

--- app/models/fortune.rb
+++ app/models/fortune.rb
@@ -2,7 +2,7 @@ class Fortune
   attr_accessor :fortunes_array
 
   def initialize
-    @fortunes_array = ["Absolutely", "Certainly", "Of course", "Uh, probably", "Yeah, mostly likely. I guess", "What are you talking about?", "Duh!", "Definitely not", "Sure thing", "What did you say?", "No.", "Absolutely not", "Not really sure, but probably not", "Why are you asking me this?", "No, no, and no", "That's a dumb question. You're an idiot"]
+    @fortunes_array = ["Absolutely,", "Certainly,", "Of course,", "Uh, probably,", "Only in your wildest dreams,", "Yeah, mostly likely. I guess,", "What are you talking about?", "Duh!", "Definitely not,", "Sure thing,", "What did you say?", "The world is cruel,", "No,", "Absolutely not,", "Not really sure, but probably not,", "Why are you asking me this?", "No, no, and no,", "That's a dumb question. You're an idiot,"]
   end
 
 end
